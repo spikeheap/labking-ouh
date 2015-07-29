79a0f9cd203ce1ef7afa56aad1acb5961d8c1d8d
Ryan Brooks <ryanbrooksis@gmail.com>
2015-07-29 15:47:45 +0100

    Adding schema for preferences, and gulp task to copy them

---
+++ app/schemas/dbscripts/postgresql/labking_ouh-1.00-2.00.sql
@@ -0,0 +1,10 @@
+
+/* labking_ouh-1.00-2.00.sql */
+
+CREATE SCHEMA labking_ouh;
+
+CREATE TABLE labking_ouh."preferences"
+(
+    "pref_id" VARCHAR(10) NOT NULL,
+    "pref_id" VARCHAR(255) NOT NULL,
+);
